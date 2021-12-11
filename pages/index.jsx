41166a0546fd3cac0f55ad4128b3077cf52648a5
dweggy <colacolaojm@hotmail.com>
2021-12-11 17:06:12 +0400

    added about me

--- pages/index.jsx
+++ pages/index.jsx
@@ -26,7 +26,11 @@ export default function Home() {
       const nameContainerTop = nameContainer.current.getBoundingClientRect().top;
       const titleTop = titleRef.current.getBoundingClientRect().top;
       const titleHeight = titleRef.current.getBoundingClientRect().height;
-      nameContainerHeight = nameContainerTop - titleTop + titleHeight / 4 ;
+
+      console.log(nameContainer.current.getBoundingClientRect())
+      console.log(titleRef.current.getBoundingClientRect())
+
+      nameContainerHeight = nameContainerTop - titleTop + titleHeight / 8 ;
     }
 
     if (scrollContainer) {
@@ -40,7 +44,7 @@ export default function Home() {
   return (
     <>
       <div style={{display: lockTitle ? 'none' : 'flex' }} className={styles.titleContainer}>
-        <h1 ref={titleRef} className={styles.text}>Jun Ming</h1>
+        <h1 ref={titleRef} className={styles.titleText}>Jun Ming</h1>
         <h3>Welcome to my website.</h3>
         
       </div>
@@ -65,10 +69,27 @@ export default function Home() {
             <img src="./images/citylayers/cityLayer0.png"/>
         </div>
         <section className={styles.mainContainer}>
-          <div ref={nameContainer} className={styles.nameContainer}>
-            <h1 style={{display: lockTitle ? 'block' : 'none' }} className={styles.text}>
+          <div ref={nameContainer} className={styles.headerContainer}>
+            <h1 style={{display: lockTitle ? 'block' : 'none' }} className={styles.titleText}>
               Jun Ming
             </h1>
+          </div>
+          <div className={styles.aboutMe}>
+            <p>
+              Hello! I'm Jun. I'm a <span className={styles.highlight}>Malaysian</span> sophomore at NYU Abu Dhabi.
+              Interested in CS & IM.
+            </p>
+            <p>
+              Outside of computers, I enjoy a good session of gongfu-style <span className={styles.highlight}>tea</span> brewing.
+              I also play a little bit of piano. Currently
+              compiling a list of the best Asian food places in Abu Dhabi.
+            </p>
+            <p>
+              Here is my <a href="#"><span className={styles.highlight}>resume</span></a>.
+            </p>
+          </div>
+          <div className={`${styles.headerContainer} ${styles.workHeader}`}>
+            <h1 className={styles.headerText}>Works</h1>
           </div>
         </section>
       </div>
